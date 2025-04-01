6c50849b2608f52f4dc62c8665bf2faeba5a6c37
vsanc1 <valeria091306@icloud.com>
2025-04-01 10:23:46 -0500

    Update final_project.py

--- final_project.py
+++ final_project.py
@@ -1,12 +1,25 @@
 import turtle as t
+import pygame
+
+pygame.init()
+
+window_width = 800
+window_height = 600
+display = pygame.display.set_mode((window_width, window_height))
+bg = pygame.image.load("background.jpg")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+# window.title("Valeria Final Project Ping Pong")
+# window.setup(800, 600)
+# window.tracer(0)
+
 
 YouScore = 0
 You2Score = 0
-
-window = t.Screen()
-window.title("Valeria Final Project Ping Pong")
-window.setup(800, 600)
-window.tracer(0)
 
 paddle_1 = t.Turtle()
 paddle_1.speed()
