696eb7e21933907b5813e98514f7a7786d079ef2
vsanc1 <valeria091306@icloud.com>
2025-04-03 11:20:52 -0500

    Update final_project.py

--- final_project.py
+++ final_project.py
@@ -37,27 +37,3 @@ while True:
 
     pygame.display.update()
     clock.tick(60)
-
-
-"""
-#I WAS GOING TO USE TURTLE FOR THIS BUT I THINK PYGAME IS THE BETTER OPTION SO I AM KEEPING THIS HERE
-# window.title("Valeria Final Project Ping Pong")
-# window.setup(800, 600)
-# window.tracer(0)
-YouScore = 0
-You2Score = 0
-
-paddle_1 = t.Turtle()
-paddle_1.speed()
-paddle_1.shape("circle")
-paddle_1.shapesize(stretch_wid=5, stretch_len=1)
-paddle_1.penup()
-paddle_1.goto()
-
-paddle_2 = t.Turtle()
-paddle_2.speed()
-paddle_2.shape("circle")
-paddle_2.shapesize(stretch_wid=5, stretch_len=1)
-paddle_2.penup()
-paddle_2.goto()
-"""
