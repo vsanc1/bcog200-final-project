f0493d2177e9b957e4e88c01870fb865b9249db9
vsanc1 <valeria091306@icloud.com>
2025-04-03 11:19:12 -0500

    bg

--- final_project.py
+++ final_project.py
@@ -1,26 +1,43 @@
 import pygame
+import sys
 
 pygame.init()
 
 # screen stuff
-window_width = 900
-window_height = 600
+width, height = 900, 600
 
-font = pygame.font.SysFont("Comfortaa")
-display = pygame.display.set_mode((window_width, window_height))
-bg = pygame.image.load("background.jpg")
+
+display = pygame.display.set_mode((900, 600))
+font = pygame.font.SysFont("Comfortaa", int(width / 20))
+bg = pygame.image.load("background2.jpg")
+bg = pygame.transform.scale(bg, (width, height))
 pygame.display.set_caption("Valeria Final Project Ping Pong")
 
-# game loop
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+
+clock = pygame.time.Clock()
+
 
 # paddles stuff
-you_1 = pygame.Rect(30, 10)
-you_2 = pygame.Rect(30, 10)
+you_1 = pygame.Rect(width - 800, int(height / 2 - 50), 10, 100)
+you_2 = pygame.Rect(width - 110, int(height / 2 - 50), 10, 100)
+
+# game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    display.fill((0, 0, 0))
+    if bg:
+        display.blit(bg, (0, 0))
+
+    pygame.draw.rect(display, "white", you_1)
+    pygame.draw.rect(display, "white", you_2)
+
+    pygame.display.update()
+    clock.tick(60)
+
 
 """
 #I WAS GOING TO USE TURTLE FOR THIS BUT I THINK PYGAME IS THE BETTER OPTION SO I AM KEEPING THIS HERE
