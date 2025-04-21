d0d72ea7514fc4c2afa9a3779b57ed121af4d31a
vsanc1 <valeria091306@icloud.com>
2025-04-21 12:51:00 -0500

    Update final_project.py

--- final_project.py
+++ final_project.py
@@ -4,57 +4,71 @@ import sys
 pygame.init()
 
 # screen stuff
-width, height = 900, 600
 
 
-display = pygame.display.set_mode((900, 600))
-font = pygame.font.SysFont("Comfortaa", int(width / 20))
-bg = pygame.image.load("background2.jpg")
-bg = pygame.transform.scale(bg, (width, height))
-pygame.display.set_caption("Valeria Final Project Ping Pong")
+def screen_setup():
+    width, height = 900, 600
 
+    display = pygame.display.set_mode((900, 600))
+    font = pygame.font.SysFont("Comfortaa", int(width / 20))
+    bg = pygame.image.load("background2.jpg")
+    bg = pygame.transform.scale(bg, (width, height))
+    pygame.display.set_caption("Valeria Final Project Ping Pong")
 
-clock = pygame.time.Clock()
+    clock = pygame.time.Clock()
 
 
 # paddles stuff
-you_1 = pygame.Rect(width - 800, int(height / 2 - 50), 10, 100)
-you_2 = pygame.Rect(width - 110, int(height / 2 - 50), 10, 100)
+def paddles():
+    you_1 = pygame.Rect(width - 800, int(height / 2 - 50), 10, 100)
+    you_2 = pygame.Rect(width - 110, int(height / 2 - 50), 10, 100)
+
 
 # game loop with attaching keys to paddles
-while True:
-    keys_pressed = pygame.key.get_pressed()
+def game_loop():
+    while True:
+        keys_pressed = pygame.key.get_pressed()
 
-    if keys_pressed[pygame.K_UP]:
-        if you_1.top > 0:
-            you_1.top -= 2
-    if keys_pressed[pygame.K_DOWN]:
-        if you_1.bottom < height:
-            you_1.bottom += 2
+        if keys_pressed[pygame.K_UP]:
+            if you_1.top > 0:
+                you_1.top -= 2
+        if keys_pressed[pygame.K_DOWN]:
+            if you_1.bottom < height:
+                you_1.bottom += 2
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-    if keys_pressed[pygame.K_w]:
-        if you_2.top > 0:
-            you_2.top -= 2
-    if keys_pressed[pygame.K_s]:
-        if you_2.bottom < height:
-            you_2.bottom += 2
+        if keys_pressed[pygame.K_w]:
+            if you_2.top > 0:
+                you_2.top -= 2
+        if keys_pressed[pygame.K_s]:
+            if you_2.bottom < height:
+                you_2.bottom += 2
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-    display.fill((0, 0, 0))
-    if bg:
-        display.blit(bg, (0, 0))
+        display.fill((0, 0, 0))
+        if bg:
+            display.blit(bg, (0, 0))
 
-    pygame.draw.rect(display, "white", you_1)
-    pygame.draw.rect(display, "white", you_2)
+        pygame.draw.rect(display, "white", you_1)
+        pygame.draw.rect(display, "white", you_2)
 
-    pygame.display.update()
-    clock.tick(60)  # fps
+        pygame.display.update()
+        clock.tick(60)  # fps
+
+
+def main():
+    screen_setup
+    paddles
+    game_loop
+
+
+if __name__ == "__main__":
+    main()
